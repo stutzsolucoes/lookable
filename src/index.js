3cd8650e6b5d905bec900a21c59212b59d052052
marcos93 <marcos10@outlook.com>
2016-01-28 15:16:08 -0200

    Mark 0

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Featured from './Featured';
-import App from './App';
-import Modal from './Modal';
-
-ReactDOM.render(<Featured/>, document.getElementById('featured'));
-ReactDOM.render(<App/>, document.getElementById('root'));
-ReactDOM.render(<Modal/>, document.getElementById('galleryItemComponent'));
